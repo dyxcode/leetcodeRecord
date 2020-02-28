6da1fc2ed1646ff93f2661014fdde6dcfd709b34
dyxcode <821440651@qq.com>
2020-02-28 14:55:31 +0800

    第三题

---
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,58 @@
+/*给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty())
+            return 0;
+        vector<int> v(s.size());
+        set<char> ch;
+        bool flag;
+        v[0] = 1;
+        flag = 1;
+        ch.insert(s[0]);
+        for (int i = 1; i < s.size(); ++i) {
+            if (flag) {
+                auto x = ch.insert(s[i]);
+                if (x.second == false) {
+                    flag = false;
+                    v[i] = v[i-1];
+                }
+                else
+                    v[i] = v[i-1] + 1;
+            }
+            else {
+                ch.clear();
+                int j;
+                for(j = i; j >= 0; --j) {
+                    auto x = ch.insert(s[j]);
+                    if (x.second == false)
+                        break;
+                }
+                if (i - j >= v[i-1]) {
+                    v[i] = i - j;
+                    flag = true;
+                }
+                else v[i] = v[i-1];
+            }
+        }
+        return v.back();
+    }
+};
