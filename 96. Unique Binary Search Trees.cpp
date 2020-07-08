df05166a1adcc2b4eb5817576c271d5d641f8988
dyxcode <821440651@qq.com>
2020-07-08 21:29:11 +0800

    第九十六题

---
+++ 96. Unique Binary Search Trees.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+	int numTrees(int n)
+	{
+		if (n == 0 || n == 1)
+			return 1;
+		vector<int> dp(n + 1);
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for (int i = 2; i <= n; ++i)
+		{
+			for (int j = 1; j <= i; ++j)
+			{
+				dp[i] += dp[j - 1] * dp[i - j];
+			}
+		}
+		return dp[n];
+	}
+};
